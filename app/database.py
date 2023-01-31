966865da3ebf48c32434d0c44acaed685af6debd
jdonic <jdoni@jdonic.fedora>
2023-01-31 01:35:22 +0100

    Polishing up database insertions

--- app/database.py
+++ app/database.py
@@ -1,5 +1,9 @@
 import sqlite3
 
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 class DatabaseHandler:
     def __init__(self) -> None:
@@ -28,21 +32,52 @@ class DatabaseHandler:
 
     def insert_category(self, name: str, parent_category: str = None) -> None:
         with self.connection:
-            self.cursor.execute(
-                """INSERT INTO category (name, parent_category)
-                                    VALUES (?,?)""",
-                (name, parent_category),
-            )
+            if parent_category:
+                result = self.cursor.execute(
+                    """SELECT name FROM category WHERE name=?""", (parent_category,)
+                ).fetchone()
+
+                if not result:
+                    logger.error(
+                        f"The parent category: {parent_category} does not exist in category table, setting it to None."
+                    )
+                    parent_category = None
+
+            try:
+                self.cursor.execute(
+                    """INSERT INTO category (name, parent_category)
+                                        VALUES (?,?)""",
+                    (name, parent_category),
+                )
+                logger.info(f"Inserted {name} into a category table.")
+            except sqlite3.IntegrityError:
+                logger.error(
+                    f"The category: {name} is already present in category table."
+                )
 
     def insert_offer(
         self, id: int, name: str, description: str, category_name: str
     ) -> None:
         with self.connection:
-            self.cursor.execute(
-                """INSERT INTO offer (id, name, description, category_name)
-                                    VALUES (?,?,?,?)""",
-                (id, name, description, category_name),
-            )
+            result = self.cursor.execute(
+                """SELECT name FROM category WHERE name=?""", (category_name,)
+            ).fetchone()
+
+            if not result:
+                logger.error(
+                    f"The category: {category_name} does not exist in category table."
+                )
+                return
+
+            try:
+                self.cursor.execute(
+                    """INSERT INTO offer (id, name, description, category_name)
+                                        VALUES (?,?,?,?)""",
+                    (id, name, description, category_name),
+                )
+                logger.info(f"Inserted offer with id: {id} into a offer table.")
+            except sqlite3.IntegrityError:
+                logger.error(f"The id: {id} is already present in offer table.")
 
     def get_categories(self) -> list:
         self.cursor.execute("SELECT * FROM category")
